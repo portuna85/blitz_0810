a4b6647f8843488124706f5fc0a9dbcd29bae59b
portuna85 <portuna85@gmail.com>
2022-08-09 16:11:56 +0900

    Board Update

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,29 +2,23 @@ const main = {
     init : function() {
         const _this = this;
 
-        // 게시글 저장
         $('#btn-save').on('click', function () {
             _this.save();
         });
-        // 게시글 수정
         $('#btn-update').on('click', function () {
             _this.update();
         });
-        // 게시글 삭제
         $('#btn-delete').on('click', function () {
             _this.delete();
         });
 
-        // 회원 수정
         $('#btn-user-modify').on('click', function () {
             _this.userModify();
         });
 
-        // 댓글 저장
         $('#btn-comment-save').on('click', function () {
             _this.commentSave();
         });
-        // 댓글 수정
         document.querySelectorAll('#btn-comment-update').forEach(function (item) {
             item.addEventListener('click', function () { // 버튼 클릭 이벤트 발생시
                 const form = this.closest('form'); // btn의 가장 가까운 조상의 Element(form)를 반환 (closest)
@@ -33,14 +27,12 @@ const main = {
         });
     },
 
-    /** 글 작성 */
     save : function () {
         const data = {
             title: $('#title').val(),
             writer: $('#writer').val(),
             content: $('#content').val()
         };
-        // 공백 및 빈 문자열 체크
         if (!data.title || data.title.trim() === "" || !data.content || data.content.trim() === "") {
             alert("공백 또는 입력하지 않은 부분이 있습니다.");
             return false;
@@ -60,7 +52,6 @@ const main = {
         }
     },
 
-    /** 글 수정 */
     update : function () {
         const data = {
             id: $('#id').val(),
@@ -90,7 +81,6 @@ const main = {
         }
     },
 
-    /** 글 삭제 */
     delete : function () {
         const id = $('#id').val();
         const con_check = confirm("정말 삭제하시겠습니까?");
@@ -113,7 +103,6 @@ const main = {
         }
     },
 
-    /** 회원 수정 */
     userModify : function () {
         const data = {
             id: $('#id').val(),
@@ -159,14 +148,12 @@ const main = {
         }
     },
 
-    /** 댓글 저장 */
     commentSave : function () {
         const data = {
             postsId: $('#postsId').val(),
             comment: $('#comment').val()
         }
 
-        // 공백 및 빈 문자열 체크
         if (!data.comment || data.comment.trim() === "") {
             alert("공백 또는 입력하지 않은 부분이 있습니다.");
             return false;
@@ -185,7 +172,6 @@ const main = {
             });
         }
     },
-    /** 댓글 수정 */
     commentUpdate : function (form) {
         const data = {
             id: form.querySelector('#id').value,
@@ -222,7 +208,6 @@ const main = {
         }
     },
 
-    /** 댓글 삭제 */
     commentDelete : function (postsId, commentId, commentWriterId, sessionUserId) {
 
         // 본인이 작성한 글인지 확인
